6bbcbf6afa83ac54314df4b48fe7b8872ee972c4
mattemoni <mattemoni@gmail.com>
2026-06-26 23:05:36 +0200

    Add Algorithm model
    https://github.com/alienix2/algo-evaluation/issues/1

---
+++ src/main/java/com/matteo/projects/algo_evaluation/model/Algorithm.java
@@ -0,0 +1,38 @@
+package com.matteo.projects.algo_evaluation.model;
+
+import java.util.Objects;
+
+public class Algorithm {
+	private String id;
+	private String name;
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	@Override
+	public String toString() {
+		return "Algorithm [id=" + id + ", name=" + name + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Algorithm other = (Algorithm) obj;
+		return Objects.equals(id, other.id) && Objects.equals(name, other.name);
+	}
+}
