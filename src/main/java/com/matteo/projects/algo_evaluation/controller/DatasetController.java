dcfe6e2f77459480049b8a4845cdebbf789d2108
mattemoni <mattemoni@gmail.com>
2026-07-02 21:56:13 +0200

    Add newDataset to DatasetController
    References #14

--- src/main/java/com/matteo/projects/algo_evaluation/controller/DatasetController.java
+++ src/main/java/com/matteo/projects/algo_evaluation/controller/DatasetController.java
@@ -1,5 +1,6 @@
 package com.matteo.projects.algo_evaluation.controller;
 
+import com.matteo.projects.algo_evaluation.model.Dataset;
 import com.matteo.projects.algo_evaluation.repository.DatasetRepository;
 import com.matteo.projects.algo_evaluation.view.AlgoEvaluationView;
 
@@ -15,6 +16,17 @@ public class DatasetController {
 	
 	public void allDatasets() {
 		datasetView.showAllDatasets(datasetRepository.findAll());
+	}
+
+	public void newDataset(Dataset dataset) {
+		Dataset existing = datasetRepository.findById(dataset.getId());
+		if (existing != null) {
+			datasetView.showDatasetError("Already existing dataset with id " + dataset.getId(), existing);
+			return;
+		}
+
+		datasetRepository.save(dataset);
+		datasetView.datasetAdded(dataset);
 	}
 
 }
--- src/test/java/com/matteo/projects/algo_evaluation/controller/DatasetControllerTest.java
+++ src/test/java/com/matteo/projects/algo_evaluation/controller/DatasetControllerTest.java
@@ -1,21 +1,22 @@
 package com.matteo.projects.algo_evaluation.controller;
 
-import static org.junit.Assert.*;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.ignoreStubs;
+import static org.mockito.Mockito.inOrder;
 
 import java.util.Arrays;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import com.matteo.projects.algo_evaluation.model.Algorithm;
 import com.matteo.projects.algo_evaluation.model.Dataset;
-import com.matteo.projects.algo_evaluation.repository.AlgorithmRepository;
 import com.matteo.projects.algo_evaluation.repository.DatasetRepository;
 import com.matteo.projects.algo_evaluation.view.AlgoEvaluationView;
 
@@ -48,5 +49,25 @@ public class DatasetControllerTest {
 		when(datasetRepository.findAll()).thenReturn(Arrays.asList(dataset));
 		datasetController.allDatasets();
 		verify(datasetView).showAllDatasets(Arrays.asList(dataset));
+	}
+
+	@Test
+	public void testNewDatasetDoesNotAlreadyExist() {
+		when(datasetRepository.findById("1")).thenReturn(null);
+		Dataset dataset = new Dataset("1", "Dataset1", Arrays.asList(1, 2, 3));
+		datasetController.newDataset(dataset);
+		InOrder inOrder = inOrder(datasetRepository, datasetView);
+		inOrder.verify(datasetRepository).save(dataset);
+		inOrder.verify(datasetView).datasetAdded(dataset);
+	}
+
+	@Test
+	public void testNewDatasetAlreadyExist() {
+		Dataset existing = new Dataset("1", "Dataset1", Arrays.asList(1, 2, 3));
+		Dataset toAdd = new Dataset("1", "Dataset2", Arrays.asList(4, 5, 6));
+		when(datasetRepository.findById("1")).thenReturn(existing);
+		datasetController.newDataset(toAdd);
+		verify(datasetView).showDatasetError("Already existing dataset with id " + toAdd.getId(), existing);
+		verifyNoInteractions(ignoreStubs(datasetRepository));
 	}
 }
