73287ecf70abb1f34eb208da3d608f946ed76576
mattemoni <mattemoni@gmail.com>
2026-07-03 16:42:57 +0200

    Add newRun to RunController
    References #15

--- src/main/java/com/matteo/projects/algo_evaluation/controller/RunController.java
+++ src/main/java/com/matteo/projects/algo_evaluation/controller/RunController.java
@@ -1,13 +1,14 @@
 package com.matteo.projects.algo_evaluation.controller;
 
+import com.matteo.projects.algo_evaluation.model.Run;
 import com.matteo.projects.algo_evaluation.repository.RunRepository;
 import com.matteo.projects.algo_evaluation.view.AlgoEvaluationView;
 
 public class RunController {
-	
+
 	private AlgoEvaluationView runView;
 	private RunRepository runRepository;
-	
+
 	public RunController(AlgoEvaluationView runView, RunRepository runRepository) {
 		this.runView = runView;
 		this.runRepository = runRepository;
@@ -15,6 +16,17 @@ public class RunController {
 
 	public void allRuns() {
 		runView.showAllRuns(runRepository.findAll());
+	}
+
+	public void newRun(Run run) {
+		Run existing = runRepository.findById(run.getId());
+		if (existing != null) {
+			runView.showRunError("Already existing run with id " + run.getId(), existing);
+			return;
+		}
+
+		runRepository.save(run);
+		runView.runAdded(run);
 	}
 
 }
--- src/test/java/com/matteo/projects/algo_evaluation/controller/RunControllerTest.java
+++ src/test/java/com/matteo/projects/algo_evaluation/controller/RunControllerTest.java
@@ -1,13 +1,17 @@
 package com.matteo.projects.algo_evaluation.controller;
 
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.ignoreStubs;
+import static org.mockito.Mockito.inOrder;
 
 import java.util.Arrays;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -45,6 +49,26 @@ public class RunControllerTest {
 		when(runRepository.findAll()).thenReturn(Arrays.asList(run));
 		runController.allRuns();
 		verify(runView).showAllRuns(Arrays.asList(run));
+	}
+	
+	@Test
+	public void testNewRunDoesNotAlreadyExist() {
+		when(runRepository.findById("1")).thenReturn(null);
+		Run run = new Run("1", "1", "1", 1);
+		runController.newRun(run);
+		InOrder inOrder = inOrder(runRepository, runView);
+		inOrder.verify(runRepository).save(run);
+		inOrder.verify(runView).runAdded(run);
+	}
+	
+	@Test
+	public void testNewRunAlreadyExist() {
+		Run existing = new Run("1", "1", "1", 1);
+		Run toAdd = new Run("1", "2", "2", 2);
+		when(runRepository.findById("1")).thenReturn(existing);
+		runController.newRun(toAdd);
+		verify(runView).showRunError("Already existing run with id " + toAdd.getId(), existing);
+		verifyNoMoreInteractions(ignoreStubs(runRepository));
 	}
 
 }
