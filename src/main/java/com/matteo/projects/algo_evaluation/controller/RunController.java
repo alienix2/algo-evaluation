8317832fdea5773f5ecaef080e3c7ef738dda737
mattemoni <mattemoni@gmail.com>
2026-07-03 16:54:08 +0200

    Add deleteRun to RunController
    - Fix naming convention for tests in DatasetControllerTest
    
    References #15

--- src/main/java/com/matteo/projects/algo_evaluation/controller/RunController.java
+++ src/main/java/com/matteo/projects/algo_evaluation/controller/RunController.java
@@ -29,4 +29,15 @@ public class RunController {
 		runView.runAdded(run);
 	}
 
+	public void deleteRun(Run run) {
+		Run existing = runRepository.findById(run.getId());
+		if (existing == null) {
+			runView.showRunError("No existing run with id " + run.getId(), run);
+			return;
+		}
+		
+		runRepository.delete(run);
+		runView.runDeleted(run);
+	}
+
 }
