a41b74fc7f70bed117edb3f69c08c35d120db2ac
mattemoni <mattemoni@gmail.com>
2026-07-06 10:15:25 +0200

    Add ListRunCommand
    References #40

---
+++ src/main/java/com/matteo/projects/algo_evaluation/view/picocli/ListRunsCommand.java
@@ -0,0 +1,24 @@
+package com.matteo.projects.algo_evaluation.view.picocli;
+
+import java.util.concurrent.Callable;
+
+import com.matteo.projects.algo_evaluation.controller.RunController;
+
+import picocli.CommandLine.Command;
+
+@Command(name = "list-runs")
+public class ListRunsCommand implements Callable<Void> {
+	
+	private final RunController runController;
+
+	public ListRunsCommand(RunController runController) {
+		this.runController = runController;
+	}
+
+	@Override
+	public Void call() {
+		runController.allRuns();
+		return null;
+	}
+
+}
---
+++ src/test/java/com/matteo/projects/algo_evaluation/view/picocli/ListRunsCommandTest.java
@@ -0,0 +1,37 @@
+package com.matteo.projects.algo_evaluation.view.picocli;
+
+import static org.mockito.Mockito.verify;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.matteo.projects.algo_evaluation.controller.RunController;
+
+public class ListRunsCommandTest {
+
+	@Mock
+    private RunController runController;
+
+    private AutoCloseable closeable;
+
+    @Before
+    public void setup() {
+        closeable = MockitoAnnotations.openMocks(this);
+    }
+
+    @After
+    public void releaseMocks() throws Exception {
+        closeable.close();
+    }
+
+    @Test
+    public void testCallShowsAllAlgorithms() {
+    	ListRunsCommand cmd = new ListRunsCommand(runController);
+        cmd.call();
+        verify(runController).allRuns();
+    }
+
+}
