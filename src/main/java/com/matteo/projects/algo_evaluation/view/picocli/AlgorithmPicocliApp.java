205bfa9abbaa26dc5c7dba4ee26dd1a118391444
mattemoni <mattemoni@gmail.com>
2026-07-06 17:14:50 +0200

    Implementing idiomatic way to handle subcommands in PicocliApp
    References #40

--- src/main/java/com/matteo/projects/algo_evaluation/view/picocli/AlgorithmPicocliApp.java
+++ src/main/java/com/matteo/projects/algo_evaluation/view/picocli/AlgorithmPicocliApp.java
@@ -1,24 +1,80 @@
 package com.matteo.projects.algo_evaluation.view.picocli;
 
+import java.time.Clock;
 import java.util.concurrent.Callable;
+
+import com.matteo.projects.algo_evaluation.algorithm.BubbleSort;
+import com.matteo.projects.algo_evaluation.algorithm.SelectionSort;
+import com.matteo.projects.algo_evaluation.algorithm.SortingAlgorithmRegistry;
+import com.matteo.projects.algo_evaluation.controller.AlgorithmController;
+import com.matteo.projects.algo_evaluation.controller.DatasetController;
+import com.matteo.projects.algo_evaluation.controller.RunController;
+import com.matteo.projects.algo_evaluation.repository.AlgorithmRepository;
+import com.matteo.projects.algo_evaluation.repository.DatasetRepository;
+import com.matteo.projects.algo_evaluation.repository.RunRepository;
+import com.matteo.projects.algo_evaluation.repository.mongo.AlgorithmMongoRepository;
+import com.matteo.projects.algo_evaluation.repository.mongo.DatasetMongoRepository;
+import com.matteo.projects.algo_evaluation.repository.mongo.RunMongoRepository;
+import com.mongodb.MongoClient;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Mixin;
+import picocli.CommandLine.Model.CommandSpec;
+import picocli.CommandLine.Spec;
 
 @Command(name = "algo-evaluation", mixinStandardHelpOptions = true, subcommands = { ListAlgorithmsCommand.class,
 		ListDatasetsCommand.class, ListRunsCommand.class, NewRunCommand.class })
 public class AlgorithmPicocliApp implements Callable<Void> {
 
+	private RunController runController;
+	private AlgorithmController algorithmController;
+	private DatasetController datasetController;
+	
+	@Spec
+	CommandSpec spec;
+
+	private AlgorithmPicocliView algorithmPicocliView;
+
+	RunController getRunController() {
+		return runController;
+	}
+
+	DatasetController getDatasetController() {
+		return datasetController;
+	}
+
+	AlgorithmController getAlgorithmController() {
+		return algorithmController;
+	}
+
 	@Mixin
 	MongoOptions mongoOptions;
 
+	MongoClient mongoClient;
+
 	@Override
 	public Void call() {
+		algorithmPicocliView = new AlgorithmPicocliView(spec.commandLine().getOut());
+
+		SortingAlgorithmRegistry registry = new SortingAlgorithmRegistry();
+		registry.register("BubbleSort", new BubbleSort());
+		registry.register("SelectionSort", new SelectionSort());
+
+		MongoClient mongoClient = new MongoClient(mongoOptions.mongoHost, mongoOptions.mongoPort);
+		AlgorithmRepository algorithmRepository = new AlgorithmMongoRepository(mongoClient, mongoOptions.dbName);
+		DatasetRepository datasetRepository = new DatasetMongoRepository(mongoClient, mongoOptions.dbName);
+		RunRepository runRepository = new RunMongoRepository(mongoClient, mongoOptions.dbName);
+
+		runController = new RunController(algorithmPicocliView, runRepository, algorithmRepository, datasetRepository,
+				registry, Clock.systemDefaultZone());
+		algorithmController = new AlgorithmController(algorithmPicocliView, algorithmRepository);
+		datasetController = new DatasetController(algorithmPicocliView, datasetRepository);
+
 		return null;
 	}
 
 	public static void main(String[] args) {
-		new CommandLine(new AlgorithmPicocliApp()).execute(args);
+		new CommandLine(new AlgorithmPicocliApp()).setExecutionStrategy(new CommandLine.RunAll()).execute(args);
 	}
 }
